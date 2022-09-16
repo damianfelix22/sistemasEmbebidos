e4b809c22b06a9579413a082e593f2f531762b69
agustin077 <agustin.viale.6@gmail.com>
2022-09-16 18:51:19 -0300

    se agrega codigo sensor dht11

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
+#include "DHT.h"
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#define DHTTYPE DHT11   
 
+const int DHTPin = 4;     // what digital pin we're connected to
+DHT dht(DHTPin, DHTTYPE);
 void setup() {
-  // put your setup code here, to run once:
+   Serial.begin(115200);
+  // Serial.println("DHTxx test!");
+  
+   dht.begin();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+   // Wait a few seconds between measurements.
+   delay(2000);
+   // Reading temperature or humidity takes about 250 milliseconds!
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+   
+   Serial.print("Humidity: ");
+   Serial.print(h);
+   Serial.print(" %\t");
+   Serial.print("Temperature: ");
+   Serial.print(t);
+   Serial.println(" *C ");
 }
