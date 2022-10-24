dbb9b1ce02265c2f8bcdc2cb409db1214bfbeaa9
agustin077 <agustin.viale.6@gmail.com>
2022-10-24 20:15:40 -0300

    dht y pantalla

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,58 @@
-#include "DHT.h"
 #include <Arduino.h>
-#include <Adafruit_Sensor.h>
-#define DHTTYPE DHT22   
 
-const int DHTPin = 4;     // what digital pin we're connected to
-DHT dht(DHTPin, DHTTYPE);
+
+
+#include <SPI.h>
+#include <Adafruit_Sensor.h> 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+#include "DHT.h"
+
+#define DHTPIN 5     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE);  
+
+
+#define i2c_Address 0x3c 
+
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+#define OLED_RESET -1 
+
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 void setup() {
-   Serial.begin(115200);
-  // Serial.println("DHTxx test!");
-  
-   dht.begin();
-}
+  Wire.begin ();
+  delay(250);
+  display.begin(i2c_Address, true);
+  dht.begin();
+
+}  
+
 void loop() {
-   // Wait a few seconds between measurements.
-   delay(2000);
-   // Reading temperature or humidity takes about 250 milliseconds!
-   float h = dht.readHumidity();
-   float t = dht.readTemperature();
-   
-   Serial.print("Humidity: ");
-   Serial.print(h);
-   Serial.print(" %\t");
-   Serial.print("Temperature: ");
-   Serial.print(t);
-   Serial.println(" *C ");
-}
+  delay(100);
+  float h = dht.readHumidity();
+  
+  float t = dht.readTemperature();
+
+
+  
+  
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE); 
+  display.setCursor(0,0);
+  display.print("humedad:");
+  display.print(h);
+  display.setCursor(0,30);
+  display.setTextSize(1);
+  display.print("temperatura:");
+  display.print(t);
+  display.display();
+}  
