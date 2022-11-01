043c4e29539de65f4698579d4017f19930b8aca5
agustin077 <agustin.viale.6@gmail.com>
2022-11-01 19:52:32 -0300

    sensor luz RGB

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int valorLDR = 0;
+int pinLDR = 4;
+
+int Rojo = 23;
+int Verde = 22;
+int Azul = 21;
+int pinLeDsensor = 5; 
+
+void setup()
+{
+  pinMode(Rojo,OUTPUT);
+  pinMode(Verde,OUTPUT);
+  pinMode(Azul,OUTPUT);
+
+  pinMode(pinLeDsensor,OUTPUT);
+  
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    valorLDR = analogRead(pinLDR);
+    Serial.println(valorLDR);
+
+    if(valorLDR >= 400)
+    {
+        digitalWrite(pinLeDsensor, LOW);
+    }
+    else
+    {
+
+        digitalWrite(pinLeDsensor, HIGH);
+    }
+        
+    analogWrite(Rojo, 0);
+    analogWrite(Verde, 50);
+    analogWrite(Azul, 250);
+
+    delay(200);
 }
